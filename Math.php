7e55be5fe36f0bc7c7f9e8ffbe233ac4aee655c2
AungKyiMindev <112634022+AungKyiMIndev@users.noreply.github.com>
2023-03-25 23:27:07 +0630

    Add files via upload

---
+++ Math.php
@@ -0,0 +1,36 @@
+<?php
+
+//floor
+$a = 5.2;
+$aa= -5.1;
+echo(floor($aa) . "<br>");
+echo(floor($a) . "<br>");
+
+//abs()
+echo(abs(6.7) . "<br>");
+echo(abs(-6.7) . "<br>");
+echo(abs(-3) . "<br>");
+echo(abs(3) . "<br>");
+
+//ceil()
+echo(ceil(0.40) . "<br>");
+echo(ceil(5.1) . "<br>");
+echo(ceil(-5.1) . "<br>");
+
+//math_func
+echo pi();  echo "<br>";
+echo min(3,2,4,2,1,0);  echo "<br>";
+echo max(2,3,4,6,2,1);  echo "<br>";
+echo sqrt(25);      echo "<br>";
+echo round(1.4);    echo "<br>";
+echo rand();        echo "<br>";
+echo rand(100, 900);    echo "<br>";
+echo(log(2) . "<br>");
+echo(log(1) . "<br>");
+echo(mt_rand(10,100));
+
+
+
+
+
+?>
